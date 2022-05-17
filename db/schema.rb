30792dcc47c55466e77d252419893cd7227af6cc
EC2 Default User <ec2-user@ip-172-31-83-11.ec2.internal>
2022-05-17 07:09:07 +0000

    簡単なログイン機能等実装

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2022_05_17_042631) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "last_name"
+    t.string "first_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.string "postcode", null: false
+    t.string "address", null: false
+    t.string "phone_number", null: false
+    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
