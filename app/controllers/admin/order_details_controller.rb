7f93ab940ea5a3f5e371d4374a84bae00dc3cf15
cookingbo <ta99v2@gmail.com>
2022-05-23 12:35:17 +0000

    [update]order_detail_controller

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,15 +1,15 @@
 class Admin::OrderDetailsController < ApplicationController
 
+
   def update
-    @order = Order.find(params[:order_id])
-    @order_detail.update(order_detail_params)
-    redirect_to admin_order_detail_path(@order_detail.id)
+
   end
+
 
   private
 
   def order_detail_params
-    params.require(:order_detail).permit(:order_id, :item_id, :amount, :price, :production_status)
+    params.require(:order_detail).permit(:production_status)
   end
 
 end
