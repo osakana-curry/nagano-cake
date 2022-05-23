cfcc780a49fc30c4b19b60f3a7640c3243a2ffed
cookingbo <ta99v2@gmail.com>
2022-05-23 14:54:13 +0000

    [Add]注文ステータスと製作ステータス紐づけ

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,6 +2,18 @@ class Admin::OrderDetailsController < ApplicationController
 
 
   def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_detail.update(order_detail_params)
+
+    if @order_detail.production_status == "製作中" #製作ステータスが「製作作中」なら入る
+			@order.update(order_status: 2) #注文ステータスを「製作中」 に更新
+
+    elsif @order.order_details.count ==  @order.order_details.where(production_status: "製作完了").count
+			@order.update(order_status: 3) #注文ステータスを「発送準備中]に更新
+    end
+
+    redirect_to admin_order_path(@order_detail.order)
 
   end
 
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,13 +5,19 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @order_detail = OrderDetail.find(params[:id])
+    @order_details = @order.order_details
   end
 
   def update
     @order = Order.find(params[:id])
+    @order_details = @order.order_details
     @order.update(order_params)
-    redirect_to admin_orders_path
+
+    if @order.order_status == "入金確認"
+      @order_details.update_all(production_status: 1)
+    end
+
+    redirect_to admin_order_path(@order)
   end
 
   private
