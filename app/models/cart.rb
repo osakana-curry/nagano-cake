05e166262b4f813e47c544944eebf2805b029b13
mee <taichibonbarx@gmail.com>
2022-05-24 07:57:21 +0000

    Add_validation

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,8 @@ class Cart < ApplicationRecord
  belongs_to :item
  belongs_to :customer
 
+ validates :customer_id, :item_id, :amount, presence: true
+
  def subtotal
     item.add_tax_price * amount
  end
