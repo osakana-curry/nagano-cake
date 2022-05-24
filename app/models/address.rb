36764aad867c809b94532312b68593bd8e273620
mee <taichibonbarx@gmail.com>
2022-05-24 05:35:40 +0000

    Update

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :customer
 
+  validates :postcode, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+
   def address_all
   '〒' + self.postcode + ' ' + self.address + ' ' + self.name
   end
