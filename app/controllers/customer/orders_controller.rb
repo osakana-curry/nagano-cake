81e0c74cf6bd81d8c254d7917efb195216ebb7e0
cookingbo <ta99v2@gmail.com>
2022-05-26 05:47:39 +0000

    [update]注文履歴一覧

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -27,7 +27,7 @@ class Customer::OrdersController < ApplicationController
 
 
   def index
-    @orders = Order.all
+    @orders = Order.order("created_at desc").page(params[:page]).per(10)
   end
 
   def show
