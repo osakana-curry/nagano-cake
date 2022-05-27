9ea23b8d14b384df3b344d411724d356bd114c37
cookingbo <ta99v2@gmail.com>
2022-05-27 09:26:06 +0000

    [update]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def top
-    @items=Item.all
-    
+    @items=Item.all.order("created_at asc")
+
     @item1 = Item.first
     @item2 = Item.second
     @item3 = Item.third
